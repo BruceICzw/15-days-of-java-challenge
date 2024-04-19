57113a9a954fca60bd5ac5bc3f51d86ba289a35b
BruceICzw <h220396z@hit.ac.zw>
2024-04-19 11:06:43 +0200

    Day 2 Complete

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+
+//imort scanner class for getting user input
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int length, width, area;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Program to Calculate area of a rectangle:\nLength: ");
+        length = scanner.nextInt();
+        System.out.println("\nWidth: ");
+        width = scanner.nextInt();
+
+        area = length * width;
+
+        System.out.println("The area of the rectangle with dimensions " + length + " x " + width + " is " + area);
+
     }
 }
