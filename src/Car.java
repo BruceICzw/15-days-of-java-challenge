aab2d43db258dc23447db50adeb0c6d1887c9491
BruceICzw <h220396z@hit.ac.zw>
2024-04-24 11:35:13 +0200

    Day 7 Complete

--- src/Car.java
+++ src/Car.java
@@ -3,6 +3,16 @@ public class Car {
     private String model;
     private int year;
 
+    public Car() {
+
+    }
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
     public String getMake() {
         return make;
     }
